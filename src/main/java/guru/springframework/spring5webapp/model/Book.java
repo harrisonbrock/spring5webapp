1be88df345e65f247e490301ebbc8b3dc7f6a784
Harrison Brock <harison@harrisonbrock.com>
2018-09-05 23:36:25 -0400

    Added Id and generate Id to Author and Book

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -1,11 +1,18 @@
 package guru.springframework.spring5webapp.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String Title;
     private String isbn;
     private String publisher;
@@ -23,6 +30,14 @@ public class Book {
         this.isbn = isbn;
         this.publisher = publisher;
         this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
