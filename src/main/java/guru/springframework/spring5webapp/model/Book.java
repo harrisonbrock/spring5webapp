9dd78604aeba77d48c8c7c7b2595f34b3c0004f3
Harrison Brock <harison@harrisonbrock.com>
2018-09-05 23:31:25 -0400

    Create Book class

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -1,4 +1,57 @@
 package guru.springframework.spring5webapp.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Book {
+    private String Title;
+    private String isbn;
+    private String publisher;
+
+    private Set<Author> authors = new HashSet<>();
+
+    public Book(String title, String isbn, String publisher) {
+        Title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+        Title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
 }
